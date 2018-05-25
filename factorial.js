39cd83b0e836ba1de6f6302d4c488986feeae36c
Roman Brito <romanbrito1@gmail.com>
2018-05-24 19:05:18 -0500

    iterative factorial

--- factorial.js
+++ factorial.js
@@ -18,6 +18,14 @@ function factorial(n) {
  }
 
 }
+// Iterative factorial
+var factorialI = function(n) {
+  var result = 1;
+  for(var i = 1; i <=n ; i++) {
+    result = result*i;
+  }
+  return result;
+};
 
 console.log(factorial(3))
 
